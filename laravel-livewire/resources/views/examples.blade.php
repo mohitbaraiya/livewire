630f9fe9a65974c0378f0e4494a250aa63483751
princesuvagiya <prince.suhagiya@shunyavkash.com>
2022-07-22 18:53:05 +0530

    validation

--- laravel-livewire/resources/views/examples.blade.php
+++ laravel-livewire/resources/views/examples.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Styles -->
         
@@ -18,10 +19,13 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-           @livewireStyles
+           
     </head>
     <body>
-       
+
+         <livewire:contact-form />
+
+        
         <livewire:scripts />
     </body>
 </html>
--- laravel-livewire/routes/web.php
+++ laravel-livewire/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\RegistrationRequest;
+use App\Http\Livewire;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('examples');
+});
+
+
+Route::post('/contact',function(RegistrationRequest $request){
+       
+       $request->validate();
 });
