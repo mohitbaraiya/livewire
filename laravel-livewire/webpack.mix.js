a39cf38d81f0ebd859f10f9424ce76ed668c7785
princesuvagiya <prince.suhagiya@shunyavkash.com>
2022-07-24 16:24:33 +0530

    tailwind set up and authantication

--- laravel-livewire/webpack.mix.js
+++ laravel-livewire/webpack.mix.js
@@ -12,5 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+
+    mix.sass('resources/sass/app.scss', 'public/css',{}, [
+
+        require("tailwindcss"),
+
+
+    ])
+    
     .sourceMaps();
