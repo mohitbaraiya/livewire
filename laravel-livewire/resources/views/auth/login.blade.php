8cd67c811d25f43c52239c03076244512939a44a
princesuvagiya <prince.suhagiya@shunyavkash.com>
2022-07-24 18:38:48 +0530

    login done

--- laravel-livewire/resources/views/auth/login.blade.php
+++ laravel-livewire/resources/views/auth/login.blade.php
@@ -1,28 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
+
 
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="max-w-md w-full space-y-8">
-            <div>
-                <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
-                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-                <p class="mt-2 text-center text-sm text-gray-600">
-                Or
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
-                </p>
-            </div>
-            <form class="mt-8 space-y-6" method="POST" action="{{ route('login') }}">
+       
+            <form class="mt-8 space-y-8 w-96" method="POST" action="{{ route('login') }}">
                 @csrf
                 <input type="hidden" name="remember" value="true">
                     <div class="rounded-md shadow-sm -space-y-px">
 
-                        <div>
+                        <div class="mb-5">
                             <label for="email-address" class="sr-only">{{ __('Email Address') }}</label>
                             <input id="email" type="email" class=" appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email addressform-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
@@ -32,7 +20,7 @@
                             @enderror
                         </div>
 
-                        <div>
+                        <div >
                             <label for="password" class="sr-only">{{ __('Password') }}</label>
                             <input id="password" type="password" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter password @error('password') is-invalid @enderror" form-control @error('password') is-invalid @enderror" name="password" plesj required autocomplete="current-password">
                                 @error('password')
@@ -57,8 +45,16 @@
                         @endif
                     </div>
                 </div>
+                    <div class="text-sm">
+                        @if (Route::has('register'))
+                                    
+                            <a class="btn btn-link font-medium text-indigo-600 hover:text-indigo-500" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    
+                        @endif
+                    </div>
+               
 
-                <div>
+                <div >
                     <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
                         <!-- Heroicon name: solid/lock-closed -->
@@ -71,7 +67,6 @@
 
                 </div>
             </form>
-        </div>
     </div>
 
 @endsection
